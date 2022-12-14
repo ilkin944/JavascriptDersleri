1d26b0d11e03b398c5706dd1a7e12252980dfb54
Ilkin <ilkin944@gmail.com>
2022-12-14 17:14:27 +0400

    JS5 elave edildi

--- JS5/app.js
+++
@@ -1,6 +0,0 @@
-getText("./data.json")
-async function getText(data) {
-    let myObject = await fetch(file);
-    let myText = await myObject.text();
-    document.getElementById("demo").innerHTML = myText;
-  }
